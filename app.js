94b840ac5e750e379dea2abe31a5ccd4af2a9799
eduardsdn <eduard.sdn@gmail.com>
2022-09-08 14:43:08 +0200

    The rows and the columns of the grid can be now dynamically updated and tiles scale inside the grid properly without overflowing it.(Thanks to setProperty)

--- app.js
+++ app.js
@@ -1,11 +1,15 @@
-gridSize = 16;
+gridSize = 64;
 
 let grid = document.querySelector('.grid');
 
 for (let i = 0; i < gridSize*gridSize; i++) {
     let square = document.createElement('div');
     square.classList.add("square");
+    grid.style.setProperty('grid-template-columns', 'repeat(' + gridSize + ', 1fr)');
+    grid.style.setProperty('grid-template-rows', 'repeat(' + gridSize + ', 1fr)');
+
     grid.appendChild(square);
+
 }
 
 console.log(grid)
