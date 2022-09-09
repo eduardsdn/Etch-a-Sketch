58e81dacc18c7ed1c376af163343375a57fd3430
eduardsdn <eduard.sdn@gmail.com>
2022-09-09 16:45:39 +0200

    Added resize button and complete styling

---
+++ README.md
@@ -0,0 +1,8 @@
+Etch-a-Sketch web app!
+Adjust size of your drawing board and have fun >-<
+
+Sources:
+--------
+Coursor: https://cur.cursors-4u.net/others/oth-2/oth198.cur
+Background image: https://www.allarts.org/2020/01/how-bob-ross-and-the-joy-of-painting-became-unexpected-heroes-in-asmr-communities/
+--------------------------------------------------------------------
--- app.js
+++ app.js
@@ -1,23 +1,48 @@
-gridSize = 64;
 
-let grid = document.querySelector('.grid');
 
-for (let i = 0; i < gridSize*gridSize; i++) {
-    let square = document.createElement('div');
-    square.classList.add("square");
-    grid.style.setProperty('grid-template-columns', 'repeat(' + gridSize + ', 1fr)');
-    grid.style.setProperty('grid-template-rows', 'repeat(' + gridSize + ', 1fr)');
-
-    grid.appendChild(square);
-
-}
-
-document.querySelectorAll('.square').forEach(item => {
-    item.addEventListener("mouseover", event => {
-        item.className = 'squareHovered'
-    })
+sizeBtn = document.querySelector('.sizeBtn');
+sizeBtn.addEventListener("click", function() {
+    document.querySelectorAll('.squareHovered').forEach(item => { 
+        item.className = "square"});
+    let gridSize = parseInt(prompt("What's the size?"));
+    if (gridSize > 100) {
+        alert('Please enter size below 100')
+        window.location.reload();
+    }
+    paint(gridSize)
 })
 
-console.log()
+
+
+
+
+function paint(size){
+    let grid = document.querySelector('.grid');
+
+    for (let i = 0; i < size*size; i++) {
+        let square = document.createElement('div');
+        square.classList.add("square");
+        grid.style.setProperty('grid-template-columns', 'repeat(' + size + ', 1fr)');
+        grid.style.setProperty('grid-template-rows', 'repeat(' + size + ', 1fr)');
+
+        grid.appendChild(square);
+
+    }
+
+    document.querySelectorAll('.square').forEach(item => {
+        item.addEventListener("mouseover", event => {
+            item.className = 'squareHovered';
+        })
+    })
+}
+
+paint()
+
+
+
+
+
+
+
 
 
